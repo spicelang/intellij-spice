2b7be7dbea073346f82cfc142ff3a08ddd366b57
Marc Auberer <marc.auberer@chillibits.com>
2024-04-11 21:31:36 +0200

    Add support for IU, IC and CL 2024.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
  * Copyright (c) 2022-2024 ChilliBits. All rights reserved.
  */
 
+import org.jetbrains.changelog.Changelog
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 fun properties(key: String) = project.findProperty(key).toString()
@@ -81,9 +82,9 @@ tasks {
 
         // Get the latest available change notes from the changelog file
         changeNotes.set(provider {
-            changelog.run {
+            changelog.renderItem(changelog.run {
                 getOrNull(properties("pluginVersion")) ?: getLatest()
-            }.toHTML()
+            }, Changelog.OutputType.HTML)
         })
     }
 
