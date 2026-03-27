1557a3fdf0818c9bf5b505b3d11817637c0cb7fc
Marc Auberer <marc.auberer@chillibits.com>
2026-03-27 18:54:07 +0100

    Upgrade kotlin-jvm to 2.3.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
 
 plugins {
     id("java")
-    kotlin("jvm") version "2.1.21"
+    kotlin("jvm") version "2.3.20"
     id("org.jetbrains.intellij.platform") version "2.13.1"
     id("org.jetbrains.changelog") version "2.5.0"
 }
